3e6e7a98187aecc15e372716d3e1d558d8834371
avishai <avishai_taashur@gap.com>
2019-09-04 11:56:41 -0700

    adding adapter and Square component

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {shallow} from 'enzyme';
-import {expect} from 'chai';
+import Adapter from 'enzyme-adapter-react-16';
+import { shallow, configure } from 'enzyme';
+import { expect } from 'chai';
+import Square from './Square';
 
+configure({ adapter: new Adapter() });
 
-
-describe('testing if class on wrapper exist', () => {
+describe('testing if class square exist', () => {
   it('will check if .square class present', () => {
-    const component = shallow(<Sqaure></Sqaure>);
+    const component = shallow(<Square></Square>);
      expect(component.exists('.square')).to.equal(true);
   })
 })
---
+++ src/Square.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Square() {
+  return (
+    <div className="square">
+      app
+    </div>
+  );
+}
+
+export default Square;
