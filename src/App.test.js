5be588a2fa93e68604bfe527107ffb587e33a7f3
avishai <avishai_taashur@gap.com>
2019-09-04 12:35:49 -0700

    adding a test for btn elems - falid

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import Adapter from 'enzyme-adapter-react-16';
-import { shallow, configure } from 'enzyme';
+import { shallow, configure , mount} from 'enzyme';
 import { expect } from 'chai';
 import Square from './components/square/Square';
 
@@ -10,12 +10,17 @@ configure({ adapter: new Adapter() });
 
 describe('testing if class square exist', () => {
   it('will check if .square class present', () => {
-    const component = shallow(<Square></Square>);
+    const component = mount(<Square></Square>);
      expect(component.exists('.square')).to.equal(true);
   })
 })
 
-
+describe('testing if button elements exist', () => {
+  it('will check if buttons exists', () => {
+    const component = shallow(<Square/>);
+    expect(component.containsMatchingElement(<button></button>)).to.equal(true);
+  })
+})
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
