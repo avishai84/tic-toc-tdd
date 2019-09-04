46eb5fecb4eadd99a35508860262780f79e84095
avishai <avishai_taashur@gap.com>
2019-09-04 12:37:01 -0700

    btn test elemnts is now passing

--- src/components/square/Square.js
+++ src/components/square/Square.js
@@ -3,7 +3,21 @@ import React from 'react';
 function Square() {
   return (
     <div className="square">
-      
+      <div className="row1">
+        <button id="0"></button>
+        <button id="1"></button>
+        <button id="2"></button>
+      </div>
+      <div className="row2">
+        <button id="3"></button>
+        <button id="4"></button>
+        <button id="5"></button>
+      </div>
+      <div className="row3">
+        <button id="6"></button>
+        <button id="7"></button>
+        <button id="8"></button>
+      </div>
     </div>
   );
 }
