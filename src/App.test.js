a4ac2467dd129a222ffec24d4c8141d74b144de2
avishai <avishai_taashur@gap.com>
2019-09-04 11:36:30 -0700

    adding first test -failed

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {shallow} from 'enzyme';
+import {expect} from 'chai';
+
+
+
+describe('testing if class on wrapper exist', () => {
+  it('will check if .square class present', () => {
+    const component = shallow(<Sqaure></Sqaure>);
+     expect(component.exists('.square')).to.equal(true);
+  })
+})
+
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
