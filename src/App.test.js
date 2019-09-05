c6a358a64929aee5a4022a19cec497a75b5fa7b5
avishai <avishai_taashur@gap.com>
2019-09-05 09:14:08 -0700

    addind Sinon

--- src/App.test.js
+++ src/App.test.js
@@ -5,6 +5,7 @@ import Adapter from 'enzyme-adapter-react-16';
 import { shallow, configure , mount} from 'enzyme';
 import { expect } from 'chai';
 import Square from './components/square/Square';
+import Board from './components/board/Board';
 
 configure({ adapter: new Adapter() });
 
@@ -40,16 +41,19 @@ describe('if property onClick point to props.onclick', () => {
 
 describe('testing user click', () => {
   let counter = 0;
+  const inc = () => counter = counter++;
   it('simulates click events on Square component', () => {
-    const component = mount(<Square onClick={counter++} className={'name_'+counter}/>);
-    expect(component.setState({count:0}));
-    console.log(component.state().debug());
-    expect(component.find('.name_0').length).to.equal(0);
+    const component = mount(<Square onClick={inc()} className={'name_'+counter}/>);
+    console.log(component.debug());
+    expect(component.find('.name_0')).to.have.lengthOf(1);
     component.find('.name_0').simulate('click');
+  
     console.log(component.debug())
-    expect(component.find('.name_1').length).to.equal(0);
+    expect(component.find('.name_1').length).to.equal(1);
   })
 })
+
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -6,41 +6,16 @@ class Board extends Component{
     constructor(props){
         super(props);
         this.state = {
-            sqr: this.props.squares,
-            nextTurnIs: this.props.nextTurnIs,
-            mark: this.props.mark
+            sqr: this.props.squares
+        
         };
     }
-    triggeredFromSquare(e){
-        if(e.target.nodeName === 'BUTTON'){
-            // this will determind which block you clicked on
-            // target the btn which was clicked
-            this.setState({
-                nextTurnIs: !this.state.nextTurnIs,
-                mark : this.state.nextTurnIs ? 'O' : 'X'
-            });
-           this.props.updateGame(e, this.state.nextTurnIs, this.state.mark);
-           // update positon in array based on id
-           this.updateSqrArr(e.target.id);
-           if(e.target.nodeName === 'BUTTON'){
-           e.target.innerText = this.props.mark;
-          }
-        }
-    }
-    updateSqrArr(idPosition){
-        // pass on data to parent Game
-        this.props.updateSqr(idPosition, this.state.mark);
-    }
+ 
 
     render(){
 
         return (
             <div className="Board">
-              <Square 
-              onClick={(e) => this.triggeredFromSquare(e)}
-              square={this.props.squares} 
-              />
-              {this.state.sqr}
          
             </div>
           );
